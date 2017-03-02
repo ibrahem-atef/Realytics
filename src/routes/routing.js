9130226fd330934384311b979d96fd25d2712274
Ibrahem Atef <ibrahematef@gmail.com>
2017-03-02 19:31:59 +0200

    implemented the router (express.Router()) pattern

---
+++ src/routes/routing.js
@@ -0,0 +1,78 @@
+var express     = require('express');
+// var app         = express();
+var bodyParser  = require("body-parser");
+var passport    = require('passport');
+var jwt         = require('jwt-simple');
+var UserModel   = require('../config/models/user');
+var config      = require('../config/database');
+
+var router = express.Router();
+
+router.get('/test', function(req, res){
+    res.send("test")
+});
+ 
+router.post('/signup', function(req, res) {
+  if (!req.body.name || !req.body.password) {
+    res.json({success: false, msg: 'Please pass name and password.'});
+  } else {
+    var newUser = new UserModel({
+      name: req.body.name,
+      password: req.body.password
+    });
+    // save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({success: false, msg: 'Username already exists.'});
+      }
+      res.json({success: true, msg: 'Successful created new user.'});
+    });
+  }
+});
+
+router.post('/authenticate', function(req, res) {
+  UserModel.findOne({
+    name: req.body.name
+  }, function(err, user) {
+    if (err) throw err;
+ 
+    if (!user) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // check if password matches
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          var token = jwt.encode(user, config.secret);
+          // return the information including token as JSON
+          res.json({success: true, token: 'JWT ' + token});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
+});
+
+router.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    // these lines seems to be useless as passport authorization already added a req.user
+    var decoded = jwt.decode(token, config.secret);
+    UserModel.findOne({
+      name: decoded.name
+    }, function(err, user) {
+        if (err) throw err;
+ 
+        if (!user) {
+          return res.status(403).send({success: false, msg: 'Authentication failed. UserModel not found.'});
+        } else {
+          res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+        }
+    });
+  } else {
+    return res.status(403).send({success: false, msg: 'No token provided.'});
+  }
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ var passport    = require('passport');
 var jwt         = require('jwt-simple');
 var UserModel   = require('./config/models/user');
 var config      = require('./config/database');
+var router      = require('./routes/routing.js');
+
 // this is a comment again
 var port = process.env.PORT || 3000;
 
@@ -28,82 +30,22 @@ app.use(passport.initialize());
 // app.use(require('passport-session'));
 require('./config/passport')(passport);
 
-app.get("/", function(request, response){
+app.use('/', router);
+
+// roue for home page
+router.get("/", function(request, response){
     // response.send("Hello World");
     response.sendFile(path.join(__dirname, './modules', 'welcome', 'welcome_view.html'));
-})
+});
 
-app.post("/", function(request, response){
+router.post("/", function(request, response){
     // response.send("Hello World");
     // var body = JSON.parse(request.body);
     response.sendFile(path.join(__dirname, './modules', 'welcome', 'welcome_view.html'));
-})
+});
 
-app.get("/login", function(request, response){
+router.get("/login", function(request, response){
     response.sendFile(path.join(__dirname, './modules', 'login', 'login_view.html'));    
-})
- 
-app.post('/signup', function(req, res) {
-  if (!req.body.name || !req.body.password) {
-    res.json({success: false, msg: 'Please pass name and password.'});
-  } else {
-    var newUser = new UserModel({
-      name: req.body.name,
-      password: req.body.password
-    });
-    // save the user
-    newUser.save(function(err) {
-      if (err) {
-        return res.json({success: false, msg: 'Username already exists.'});
-      }
-      res.json({success: true, msg: 'Successful created new user.'});
-    });
-  }
-});
-
-app.post('/authenticate', function(req, res) {
-  UserModel.findOne({
-    name: req.body.name
-  }, function(err, user) {
-    if (err) throw err;
- 
-    if (!user) {
-      res.send({success: false, msg: 'Authentication failed. User not found.'});
-    } else {
-      // check if password matches
-      user.comparePassword(req.body.password, function (err, isMatch) {
-        if (isMatch && !err) {
-          // if user is found and password is right create a token
-          var token = jwt.encode(user, config.secret);
-          // return the information including token as JSON
-          res.json({success: true, token: 'JWT ' + token});
-        } else {
-          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
-        }
-      });
-    }
-  });
-});
-
-app.get('/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
-  var token = getToken(req.headers);
-  if (token) {
-    // these lines seems to be useless as passport authorization already added a req.user
-    var decoded = jwt.decode(token, config.secret);
-    UserModel.findOne({
-      name: decoded.name
-    }, function(err, user) {
-        if (err) throw err;
- 
-        if (!user) {
-          return res.status(403).send({success: false, msg: 'Authentication failed. UserModel not found.'});
-        } else {
-          res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
-        }
-    });
-  } else {
-    return res.status(403).send({success: false, msg: 'No token provided.'});
-  }
 });
 
 // serve static files for the angular app 'module' dashboard
