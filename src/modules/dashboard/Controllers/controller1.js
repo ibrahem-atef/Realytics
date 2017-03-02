0d29983dbe191ce47311301a4fb431df79410422
Ibrahem Atef <ibrahematef@gmail.com>
2017-03-02 16:51:01 +0200

    removed all unnecessary files

--- src/modules/dashboard/Controllers/controller1.js
+++ src/modules/dashboard/Controllers/controller1.js
@@ -1,16 +1,9 @@
 angular.module("myApp").controller("c1", function($scope, JSONget){
-    $scope.viewName = "view one";
-
-    $scope.ShowInfo = function(data){
-        a = data;
-        $scope.selected = data;
-    }
 
     JSONget.GetAll("https://api.github.com/users", successHandler, failureHandler);
 
     function successHandler(response){
             $scope.myData = response.data;
-            console.log(response.data);
     }
 
     function failureHandler(){
--- src/modules/dashboard/app.js
+++ src/modules/dashboard/app.js
@@ -2,5 +2,5 @@ angular.module("myApp", ["ngRoute"]);
 
 angular.module("myApp").config(function($routeProvider, $locationProvider){
     $locationProvider.hashPrefix('');
-    $routeProvider.when("/", {templateUrl: "Views/one.html", controller: "c1"});
+    $routeProvider.when("/", {templateUrl: "Views/view1.html", controller: "c1"});
 })
