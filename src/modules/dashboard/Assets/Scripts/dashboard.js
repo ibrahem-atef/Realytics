480335c8a7662384a4f0a8b036ac43cda4940fce
Ibrahem Atef <ibrahematef@gmail.com>
2017-03-03 09:23:10 +0200

    on focus out for the navbar

--- src/modules/dashboard/Assets/Scripts/dashboard.js
+++ src/modules/dashboard/Assets/Scripts/dashboard.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $(".nav a").on("click", function(){
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
+        $(".nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+    }).on("focusout", function(){
+        $(this).parent().removeClass("active");
     })
 })
