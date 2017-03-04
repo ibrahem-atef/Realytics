44516ea30e9e76b9608331857873ee3d753efffa
Ibrahem Atef <ibrahematef@gmail.com>
2017-03-04 21:55:29 +0200

    added a test route for the map data

--- src/routes/routing_site_data.js
+++ src/routes/routing_site_data.js
@@ -18,6 +18,31 @@ router.get('/', function(req, res){
                 ['2016',  1030,      540]
                 ]
         );
-})
+});
+
+router.get('/map', function(req, res){
+    res.json(
+                // [
+                // ['City', 'Hits'],
+                // ['Cairo, EG',  1000],
+                // ['Alexandrea, EG',  1170],
+                // ['Giza, EG',  660],
+                // ]
+                [
+                ['Country',   'Population', 'Area'],
+                ['Egypt',      2761477,    1285.31],
+                ['South Africa',     1324110,    181.76],
+                ['Kenya',    959574,     117.27],
+                ['Canada',     907563,     130.17],
+                ['US',   655875,     158.9],
+                ['France',     607906,     243.60],
+                ['RU',   380181,     140.7],
+                ['UK',  371282,     102.41],
+                ['Australia', 67370,      213.44],
+                ['Saudi Arabia',     52192,      43.43],
+                ['Italy',  38262,      11]
+                ]
+        );
+});
 
 module.exports = router;
