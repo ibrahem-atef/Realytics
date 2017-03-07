3427bd23ebc4e51e970a309ff16de7540c503f54
Ibrahem Atef <ibrahematef@gmail.com>
2017-03-08 01:46:26 +0200

    included the signout functionality

--- src/modules/dashboard/app.js
+++ src/modules/dashboard/app.js
@@ -4,3 +4,9 @@ angular.module("myApp").config(function($routeProvider, $locationProvider){
     $locationProvider.hashPrefix('');
     $routeProvider.when("/", {templateUrl: "Views/view1.html", controller: "c1"});
 })
+
+function SignOut(){
+    console.log(Cookies.get('authorization'));
+    Cookies.remove('authorization');
+    window.location.replace("/login");
+}
